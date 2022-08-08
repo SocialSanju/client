219b0a610f2ae9551156ad44e2b625176f21f5d4
SocialSanju <108932501+SocialSanju@users.noreply.github.com>
2022-08-08 18:36:32 +0530

    Update userActions.js

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -11,7 +11,7 @@ import { USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNOU
 export const register = (user_name, user_id) => async (dispatch) => {
   dispatch({ type: USER_REGISTER_REQUEST, payload: { user_name, user_id } });
   try {
-    const { data } = await Axios.post('/api/users/register', { user_name, user_id });
+    const { data } = await Axios.post('http://54.242.166.223:8080/api/users/register', { user_name, user_id });
     dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
     dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
     localStorage.setItem('userInfo', JSON.stringify(data));
@@ -29,7 +29,7 @@ export const register = (user_name, user_id) => async (dispatch) => {
 export const signin = (user_id) => async (dispatch) => {
   dispatch({ type: USER_SIGNIN_REQUEST, payload: { user_id } });
   try {
-    const { data } = await Axios.post('/api/users/signin', { user_id });
+    const { data } = await Axios.post('http://54.242.166.223:8080/api/users/signin', { user_id });
     dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
     localStorage.setItem('userInfo', JSON.stringify(data));
   } catch (error) {
@@ -55,7 +55,7 @@ export const detailsUser = (userId) => async (dispatch, getState) => {
     userSignin: { userInfo },
   } = getState();
   try {
-    const { data } = await Axios.get(`/api/users/${userId}`, {
+    const { data } = await Axios.get(`http://54.242.166.223:8080/api/users/${userId}`, {
       headers: { Authorization: `Sanju ${userInfo?.token}` },
     });
     dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
@@ -74,7 +74,7 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
     userSignin: { userInfo },
   } = getState();
   try {
-    const { data } = await Axios.put(`/api/users/profile`, user, {
+    const { data } = await Axios.put(`http://54.242.166.223:8080/api/users/profile`, user, {
       headers: { Authorization: `Sanju ${userInfo.token}` },
     });
     dispatch({ type: USER_UPDATE_PROFILE_SUCCESS, payload: data });
@@ -95,7 +95,7 @@ export const listUsers = () => async (dispatch, getState) => {
     const {
       userSignin: { userInfo },
     } = getState();
-    const { data } = await Axios.get('/api/users', {
+    const { data } = await Axios.get('http://54.242.166.223:8080/api/users', {
       headers: {
         Authorization: `Sanju ${userInfo.token}`,
       },
@@ -116,7 +116,7 @@ export const deleteUser = (userId) => async (dispatch, getState) => {
     userSignin: { userInfo },
   } = getState();
   try {
-    const { data } = await Axios.delete(`/api/users/${userId}`, {
+    const { data } = await Axios.delete(`http://54.242.166.223:8080/api/users/${userId}`, {
       headers: { Authorization: `Sanju ${userInfo.token}` },
     });
     dispatch({ type: USER_DELETE_SUCCESS, payload: data });
@@ -135,7 +135,7 @@ export const updateUser = (user) => async (dispatch, getState) => {
     userSignin: { userInfo },
   } = getState();
   try {
-    const { data } = await Axios.put(`/api/users/${user._id}`, user, {
+    const { data } = await Axios.put(`http://54.242.166.223:8080/api/users/${user._id}`, user, {
       headers: { Authorization: `Sanju ${userInfo.token}` },
     });
     dispatch({ type: USER_UPDATE_SUCCESS, payload: data });
